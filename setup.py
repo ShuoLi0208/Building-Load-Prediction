c725472ae555b64d84e7e9b3628ebcc3b3028847
ShuoLi0208 <shuo.li0208@gmail.com>
2023-04-11 20:35:58 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+# setup.py is used to 
+
+from setuptools import find_packages, setup
+from typing import List
+
+
+hyphen_e_dot = '-e .'
+def get_requirements(file_path: str) -> List[str]:
+    """
+    this function returns the list of requirements
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines() # readlines() method returns a list containing each line in the file as a list item
+        requirements = [req.replace("\n", "") for req in requirements] # replace \n with blank
+
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+
+setup(
+name='bulding-load-prediction',
+version='0.0.1',
+author='Shuo Li',
+author_email='shuo.li0208@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
