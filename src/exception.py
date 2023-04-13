37a67ccf0d6549fe150ad2884e2e801343d9cd90
ShuoLi0208 <shuo.li0208@gmail.com>
2023-04-13 15:04:41 -0400

    notebook

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
-import logging 
-import logger
+from src.logger import logging 
 
 
 def error_message_detail(error, error_detail:sys):
@@ -23,10 +22,10 @@ class CustomException(Exception):
         return self.error_message
 
 
-# if __name__=="__main__":
+if __name__=="__main__":
 
-#     try:
-#         a = 1/0
-#     except Exception as e:
-#         logging.info("Division by zero")
-#         raise CustomException(e, sys)
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Division by zero")
+        raise CustomException(e, sys)
