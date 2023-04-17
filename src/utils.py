34ea3e07c0870470ca3d1d0836f517b51eba4908
ShuoLi0208 <shuo.li0208@gmail.com>
2023-04-17 09:14:35 -0400

    updated utils file

--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,11 @@ import numpy as np
 import pandas as pd
 import pickle
 
+from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_absolute_percentage_error
+
 from src.exception import CustomException
 
+# save preprocessing and model.pkl files
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -17,3 +20,22 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
+# load preprocessing and model.pkl files
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            obj = pickle.load(file_obj)
+        
+        return obj
+        
+    except Exception as e:
+        raise CustomException(e, sys)
+
+# list model performance metrics
+def evaluate_model(true, pred):
+    mae = mean_absolute_error(true, pred)
+    mse = mean_squared_error(true, pred)
+    rmse = np.sqrt(mse)
+    mape = mean_absolute_percentage_error(true, pred)
+
+    return mae, rmse, mape
